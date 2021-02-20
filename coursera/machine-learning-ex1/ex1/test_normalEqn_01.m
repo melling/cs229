61bf45dcd69ec4e7cef5d1cced066af7e070e53a
Michael Mellinger <mmellinger66@gmail.com>
2021-02-20 11:43:01 -0500

    7 out of 7

--- coursera/machine-learning-ex1/ex1/test_normalEqn_01.m
+++ coursera/machine-learning-ex1/ex1/test_normalEqn_01.m
@@ -1,8 +1,11 @@
 % 
+
+% Test Case: https://www.coursera.org/learn/machine-learning/discussions/all/threads/5wftpZnyEeWKNwpBrKr_Fw
+
 X = [ 2 1 3; 7 1 9; 1 8 1; 3 7 4 ];
 y = [2 ; 5 ; 5 ; 6];
 theta = normalEqn(X,y)
-
+% inv(X' * X) * X' * y
 % theta 
 %    0.0083857
 %    0.5681342
