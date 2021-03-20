2d8a83210941a6dc8cc1969058650788135aa5e6
Michael Mellinger <mmellinger66@gmail.com>
2021-03-20 16:23:19 -0400

    Adding test cases

---
+++ coursera/machine-learning-ex4/ex4/test_SigmoidGradient.m
@@ -0,0 +1,9 @@
+%
+% https://www.coursera.org/learn/machine-learning/discussions/weeks/5/threads/oRjv1jr2EeWm9SIAC5HAew
+
+sigmoidGradient([[-1 -2 -3] ; magic(3)])
+% ans =
+%   1.9661e-001  1.0499e-001  4.5177e-002
+%   3.3524e-004  1.9661e-001  2.4665e-003
+%   4.5177e-002  6.6481e-003  9.1022e-004
+%   1.7663e-002  1.2338e-004  1.0499e-001
---
+++ coursera/machine-learning-ex4/ex4/test_nnCostFunction.m
@@ -0,0 +1,36 @@
+
+% Test Case: 
+% https://www.coursera.org/learn/machine-learning/discussions/weeks/5/threads/uPd5FJqnEeWWpRIGHRsuuw
+
+il = 2;              % input layer
+hl = 2;              % hidden layer
+nl = 4;              % number of labels
+nn = [ 1:18 ] / 10;  % nn_params
+X = cos([1 2 ; 3 4 ; 5 6]);
+y = [4; 2; 3];
+
+lambda = 0; % Unregularized
+% [J grad] = nnCostFunction(nn, il, hl, nl, X, y, lambda)
+[J grad] = nnCostFunction(nn, il, hl, nl, X, y, 0) 
+
+% J=7.4070
+
+% grad =
+%    0.766138
+%    0.979897
+%   -0.027540
+%   -0.035844
+%   -0.024929
+%   -0.053862
+%    0.883417
+%    0.568762
+%    0.584668
+%    0.598139
+%    0.459314
+%    0.344618
+%    0.256313
+%    0.311885
+%    0.478337
+%    0.368920
+%    0.259771
+%    0.322331
